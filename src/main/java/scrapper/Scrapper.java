6779691bfc85a2bf4885b01def4da9309635d5e5
stefanpetrovic <petrovicstefan91@gmail.com>
2017-06-06 10:16:50 +0200

    Extending scrapper

--- src/main/java/scrapper/Scrapper.java
+++ src/main/java/scrapper/Scrapper.java
@@ -2,25 +2,47 @@ package scrapper;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Iterator;
+
 @SpringBootApplication
 public class Scrapper {
+
+    private static final Logger log = LoggerFactory.getLogger(Scrapper.class);
 
     public static void main(String[] args) {
         RestTemplate restTemplate = new RestTemplate();
 
         String page = restTemplate.getForObject("https://www.halooglasi.com/nekretnine/prodaja-stanova/beograd?cena_d_to=60000&cena_d_unit=4&kvadratura_d_from=60&kvadratura_d_unit=1", String.class);
 
+        System.out.println("Fetched page");
+
         Document doc = Jsoup.parse(page);
 
-        System.out.println(doc.charset());
+        doc.getElementsByTag("script").remove();
 
-        Elements elems = doc.getElementsByClass("product-item");
+        Elements elems = doc.getElementsByAttribute("data-value");
 
-        System.out.println(elems.size());
+        Iterator<Element> iterable = elems.iterator();
+        while (iterable.hasNext()) {
+
+            Element el = iterable.next();
+            try {
+                Double price = Double.parseDouble(el.attr("data-value"));
+            } catch (NumberFormatException ex) {
+                log.info("Failed to convert price: ", ex.getMessage());
+                log.info("Element: {}", el);
+            }
+            System.out.println();
+        }
+
+
     }
 
 }

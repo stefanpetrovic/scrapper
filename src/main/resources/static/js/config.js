b396840882b2083a73d58d47fb7b8d7502b05305
stefanpetrovic <petrovicstefan91@gmail.com>
2017-06-15 16:57:44 +0200

    Enabled angular

--- src/main/resources/static/js/config.js
+++ src/main/resources/static/js/config.js
@@ -1,10 +1,12 @@
-angular.module('scrapperApp').config(['$routeProvider', function($routeProvider) {
+angular.module('scrapperApp').config(['$routeProvider', function ($routeProvider) {
 
-    $routeProvider.when('/', {
-        templateUrl: 'views/management.html'
-    });
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/management.html'
+        })
+        .when('#/management', {
+            templateUrl: 'views/management.html'
+        });
 
-    $routeProvider.when('/management', {
-        templateUrl: 'views/management.html'
-    });
+
 }]);

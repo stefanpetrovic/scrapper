373a838fcbb9fd6c463f1c9963037e6eb6a6c36a
stefan <petrovicstefan91@gmail.com>
2017-09-28 23:35:25 +0200

    init

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2,6 +2,13 @@ var scrapperApp = angular.module('scrapperApp', [
     'ngRoute',
     'ngResource',
     'blockUI',
-    'ui.bootstrap'
+    'ui.bootstrap',
+    'ngTagsInput'
 ]);
 
+scrapperApp.filter('htmlToPlaintext', function() {
+        return function(text) {
+            return  text ? String(text).replace(/<[^>]+>|&nbsp;/gm, '') : '';
+        };
+    }
+);

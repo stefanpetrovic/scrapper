3f98fd8b6b3e21888dc2c25eeddee03afdc30833
stefanpetrovic <petrovicstefan91@gmail.com>
2017-12-25 14:28:05 +0100

    n

--- src/main/java/scrapper/analyzer/ApartmentAnalyzer.java
+++ src/main/java/scrapper/analyzer/ApartmentAnalyzer.java
@@ -1,7 +1,5 @@
 package scrapper.analyzer;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import scrapper.model.Apartment;
@@ -14,14 +12,11 @@ import scrapper.service.ApartmentRecommender;
 import scrapper.service.ApartmentStorage;
 import scrapper.service.RecommenderConfigService;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
 public class ApartmentAnalyzer {
-
-    private static final Logger log = LoggerFactory.getLogger(ApartmentAnalyzer.class);
 
     @Autowired
     private ApartmentStorage apartmentStorage;
@@ -33,8 +28,6 @@ public class ApartmentAnalyzer {
     private RecommenderConfigService recommenderConfigService;
 
     public List<Apartment> analyzeApartments(List<Apartment> apartments, ProcessingMode processingMode) {
-        List<Apartment> recommendedApartments = new ArrayList<>();
-
         ApartmentRecommender apartmentRecommender = getApartmentRecommender(processingMode);
 
         for (Apartment a : apartments) {
@@ -43,12 +36,6 @@ public class ApartmentAnalyzer {
             boolean isRecommended = response.isRecommended();
             a.setRecommended(isRecommended);
             a.setMessage(response.getMessage());
-
-            if (isRecommended) {
-                recommendedApartments.add(a);
-            } else {
-                log.debug("Apartment not recommended: [url: {}, reason: {}]", a.getUrl(), response.getMessage());
-            }
         }
 
         return apartmentStorage.storeApartments(apartments);
